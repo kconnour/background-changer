a52dbbd8204f168e8d95ffeb43e13f91680e7828
Kyle <kyle.d.connour@gmail.com>
2023-03-18 11:53:43 -0600

    Add support for seasonal folders

--- background_changer.py
+++ background_changer.py
@@ -4,11 +4,40 @@ import os
 from pathlib import Path
 
 
-def change_background(img_dir: str, dark_mode=True):
+def get_seasonal_folder():
+    current_day_of_year = dt.date.today().timetuple().tm_yday
+    day_of_year_boundaries = (
+        dt.date(2022, 1, 1).timetuple().tm_yday,
+        dt.date(2022, 3, 21).timetuple().tm_yday,
+        dt.date(2022, 6, 21).timetuple().tm_yday,
+        dt.date(2022, 9, 21).timetuple().tm_yday,
+        dt.date(2022, 10, 1).timetuple().tm_yday,
+        dt.date(2022, 11, 1).timetuple().tm_yday,
+        dt.date(2022, 12, 21).timetuple().tm_yday,
+        dt.date(2022, 12, 31).timetuple().tm_yday + 1,
+    )
+    folders = (
+        'winter',
+        'spring',
+        'summer',
+        'fall',
+        'halloween',
+        'fall',
+        'winter'
+    )
+    for day in range(len(day_of_year_boundaries)-1):
+        if day_of_year_boundaries[day] <= current_day_of_year < day_of_year_boundaries[day+1]:
+            return folders[day]
+
+
+def change_background(img_dir: Path, dark_mode=True):
     os.environ["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
-    image_paths = sorted(Path(img_dir).glob('*'))
+    image_location = img_dir / get_seasonal_folder()
+    image_paths = sorted(image_location.glob('*'))
     weekday_number = dt.datetime.today().weekday()  # 0 is Monday
     daily_filepath = image_paths[weekday_number]
+
+    print(daily_filepath)
 
     # This was added in Ubuntu 22.04 with Wayland. Previous Ubuntus don't have the dark mode as an option
     key = '-dark' if dark_mode else ''
@@ -21,5 +50,5 @@ if __name__ == '__main__':
     # Then add '0 0 * * * /path/to/python3 /path/to/script.py' to cron to
     # change it at midnight every day
 
-    spring = '/home/kyle/Pictures/desktop_backgrounds/spring'
-    change_background(spring)
+    pictures = Path('/home/kyle/Pictures/desktop-backgrounds/')
+    change_background(pictures)
